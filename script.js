dc87e126336efe62f61dc3d5e43ad1f36b93d4f6
Adam <adam.conkle@gmail.com>
2025-05-08 13:33:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create a modal for each element. The modal has buttons to copy the code. 
-create elementByClass 
+create elementByClass and add to <main:content>
   <article>
     <img>
     <p>
