61ee4495820d567481bf330e363796463e6da5be
Adam <adam.conkle@gmail.com>
2025-05-08 13:46:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-// Create a modal for each element. The modal has buttons to copy the code. 
+/* Create a modal for each element. The modal has buttons to copy the code. 
 create elementByClass and add to <main:content>
   <article>
     <img>
     <p>
     <button:copy>
 
-
   function to add articles to navbar, maybe alphabetize?
+*/
+
+// MODAL TEMPLATE CODE 
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
